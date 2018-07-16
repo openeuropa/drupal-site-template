b8d954d393a060ca3edef2722e014a2364fdc3f9
Pieter Frenssen <pieter@frenssen.be>
2018-07-16 21:50:14 +0300

    OPENEUROPA-9: Remove deprecated SnippetAcceptingContext.

--- src/Context/FeatureContext.php
+++ src/Context/FeatureContext.php
@@ -4,18 +4,20 @@ declare(strict_types = 1);
 
 namespace OpenEuropa\my_site\Context;
 
-use Behat\Behat\Context\SnippetAcceptingContext;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
 /**
  * Defines step definitions that are generally useful for the project.
  */
-class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+class FeatureContext extends RawDrupalContext {
 
   /**
    * Checks that a 200 OK response occurred.
    *
    * @Then I should get a valid web page
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *   Thrown when a different HTTP response code was returned.
    */
   public function assertSuccessfulResponse(): void {
     $this->assertSession()->statusCodeEquals(200);
@@ -25,6 +27,9 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    * Checks that a 403 Access Denied error occurred.
    *
    * @Then I should get an access denied error
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *   Thrown when a different HTTP response code was returned.
    */
   public function assertAccessDenied(): void {
     $this->assertSession()->statusCodeEquals(403);
